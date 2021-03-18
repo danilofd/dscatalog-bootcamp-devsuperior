673817df3e68e82c9f83946e23abe2a11ddda0d5
Danilo Ferreira <warlok@live.com>
2021-03-17 20:28:59 -0400

    Update category

---
+++ backend/src/main/java/com/devsuperior/dscatalog/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsuperior.dscatalog.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
