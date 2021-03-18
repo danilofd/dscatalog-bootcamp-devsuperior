a58b414787e4fae828ec8d8ed7cbacad2ba19aa3
Danilo Ferreira <warlok@live.com>
2021-03-17 21:27:48 -0400

    Pagination

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/CategoryRepository.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
