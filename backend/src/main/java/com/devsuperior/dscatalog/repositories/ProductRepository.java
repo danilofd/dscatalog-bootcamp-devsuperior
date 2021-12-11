92033b7e9b76ce93defef196c5b21fd404c37d0e
Danilo <warlok@live.com>
2021-12-11 18:23:11 -0500

    Add swagger and product filters.

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.dscatalog.repositories;
 
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("SELECT DISTINCT p FROM Product p " +
+            "INNER JOIN p.categories c " +
+            "WHERE (:category is NULL OR :category IN c)" +
+            "AND (LOWER(p.name) LIKE LOWER(CONCAT('%',:name,'%')) )")
+    Page<Product> find(Category category, String name, Pageable pageable);
+
 }
